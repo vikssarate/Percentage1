cd4524874cbfe879e027c99e6be7f4f6f8911a31
vikssarate <viksarate01@gmail.com>
2025-08-21 09:16:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+// sw (v1) for exam practice demo
+const CACHE = 'exam-practice-v40'; // 🔹 Bump version to v10 so old cache is replaced
 
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll([
+            './',
+            './index.html',
+            './questions.json?v=img40', // 🔹 Also bump query param to force reload of JSON
+            './images/beam_ud1.png',
+            './images/Vu_m.png'
+        ]))
+    );
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
