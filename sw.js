3fe50ebc01e56281f96f212e1128fc7b9c53da4b
Ves <viksarate01@gmail.com>
2025-08-23 10:27:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,34 @@
-// sw (v1) for exam practice demo
-const CACHE = 'exam-practice-v70'; // 🔹 Bump version to v10 so old cache is replaced
+const CACHE = 'exam-practice-v73';
 
 self.addEventListener('install', e => {
-    self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE).then(c => c.addAll([
-            './',
-            './index.html',
-            './questions.json?v=img70', // 🔹 Also bump query param to force reload of JSON
-        
-            './images/vu1.jpeg',
-            './images/vu2.jpeg',
-            './images/vu3.jpeg',
-            './images/vu4.jpeg',
-            './images/vu5.jpg',
-            './images/vu6.jpg',
-            './images/vu7.jpg'
-            
-        ]))
-    );
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      './',
+      './index.html',
+      './questions.json',            // << no query
+      './images/vu1.jpeg',
+      './images/vu2.jpeg',
+      './images/vu3.jpeg',
+      './images/vu4.jpeg',
+      './images/vu5.jpg',
+      './images/vu6.jpg',
+      './images/vu7.jpg'
+    ]))
+  );
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-        )
-    );
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request /*, { ignoreSearch: true }*/).then(r => r || fetch(e.request))
+  );
 });
